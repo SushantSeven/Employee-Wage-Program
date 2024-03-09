151fc649646afd6e9d005a6739cc845ad16024de
SushantSeven <sushant.das166@gmai.com>
2024-03-09 16:42:09 +0530

    [Sushant] Add: Python script to calculate daily wage for part time

--- emp.py
+++ emp.py
@@ -1,20 +1,33 @@
 import random
 
-def check_attendance():
-    check_attd = random.randint(0,1)
-    if check_attd == 1:
-        return f"The employee is present.", calculate_daily_wage(check_attd)
-    else:
-        return f"The employee is absent.", calculate_daily_wage(check_attd)
+# wage and working hours are declared globaly
+wage = 20
+hours = 8
 
-def calculate_daily_wage(check_att):
-    wage = 20
-    hrs = 8
-    return f"The wage of employee is {wage*hrs}Rs. " if check_att == 1 else f"The wage of employee is 0 Rs. "
+# function to check the attendance of an employee
+def check_attendance():
+    check_attd = random.randint(0,2) # variable that holds the random choice to check attendance
+    if check_attd == 0:
+        return f"The employee is present full day.", check_attd # if 0 than employee is present full day 
+    elif check_attd ==1:
+        return f"The employye is present half day.", check_attd # if 1 than employee is present half day
+    else:
+        return f"The employee is absent.", check_attd # if 2 than employee is absent
+
+# function to calculate the daily wage of an employee   
+def calculate_daily_wage():
+    presence = check_attendance() # check_attendance method is called
+    if presence[1] ==0:
+        total_wage = wage * hours # total wage is calculated
+    elif presence[1] == 1:
+        total_wage = wage * (hours/2)
+    else:
+        total_wage = 0
+    return presence[0] , str(total_wage) # the total wage and whether the employee is present of absent is r returned
 
 
 if __name__=="__main__":
-    result = check_attendance()
-    print("\n".join(result))
+    result = calculate_daily_wage()
+    print(f"\n{result[0]} \n\nThe total wage for the day is Rs.{result[1]}/-\n")
+   
     
-                    
