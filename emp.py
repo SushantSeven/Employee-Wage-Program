f547e9bc09c0501fb4d4f40274948bf324b19039
SushantSeven <sushant.das166@gmai.com>
2024-03-10 18:15:16 +0530

    [Sushant] Refactor: Added main method to calculate daily wage program

--- emp.py
+++ emp.py
@@ -1,5 +1,6 @@
 import random
 
+# method to check attendance of an employee
 def check_attendance():
     check_attd = random.randint(0,1)
     if check_attd == 1:
@@ -7,11 +8,13 @@ def check_attendance():
     else:
         return f"The employee is absent.", calculate_daily_wage(check_attd)
 
+# method to calculate daily wage of the employee
 def calculate_daily_wage(check_att):
     wage = 20
     hrs = 8
     return f"The wage of employee is {wage*hrs}Rs. " if check_att == 1 else f"The wage of employee is 0 Rs. "
 
+# main method 
 def main():
     result = check_attendance()
     print("\n".join(result))
