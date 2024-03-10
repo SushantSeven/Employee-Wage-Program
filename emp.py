16882927699703aeba701ef46c2e90b4ba260293
SushantSeven <sushant.das166@gmai.com>
2024-03-10 18:08:28 +0530

    [Sushant] Refactor: Added main method

--- emp.py
+++ emp.py
@@ -1,9 +1,14 @@
 import random
 
+# method to check attendance of an employee
 def attendance(flag):
     return f"Employee is present!" if flag == 0 else f"Employee is absent!"
 
+# main method to drive other methods
+def main():
+    print(attendance(random.randint(0,1)))
 
 if __name__=="__main__":
-    print(attendance(random.randint(0,1)))
+    main()
+    
 
